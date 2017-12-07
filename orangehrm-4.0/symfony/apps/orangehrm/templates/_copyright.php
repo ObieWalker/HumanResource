0054a4e6183a54995aa06a02cc2fd67dd10c830e
chidi1 <chidi@lotusbetaanalytics.com>
2017-12-07 15:13:42 +0100

    First commit

---
+++ orangehrm-4.0/symfony/apps/orangehrm/templates/_copyright.php
@@ -0,0 +1,12 @@
+<?php 
+$version = '4.0';
+$prodName = 'OrangeHRM';
+$copyrightYear = date('Y');
+
+?>
+<?php 
+&copy;<?php echo $copyrightYear;?> <a href="http://www.lotusbetaanalytics.com" target="_blank">Lotus Beta Analytics Limited</a>. All rights reserved.
+
+<!-- <?php  $prodName . ' ' . $version;?><br/>
+&copy; 2005 - <?php  $copyrightYear;?> <a href="http://www.orangehrm.com" target="_blank">OrangeHRM, Inc</a>. All rights reserved.
+ -->
---
+++ symfony/apps/orangehrm/templates/_copyright.php
@@ -0,0 +1,10 @@
+<?php 
+$version = '4.0';
+$prodName = 'OrangeHRM';
+$copyrightYear = date('Y');
+
+?>
+
+&copy;<?php echo $copyrightYear;?> <a href="http://www.lotusbetaanalytics.com" target="_blank">Lotus Beta Analytics Limited</a>. All rights reserved.
+<!-- <?php echo $prodName . ' ' . $version;?><br/>
+&copy; 2005 - <?php echo $copyrightYear;?> <a href="http://www.orangehrm.com" target="_blank">OrangeHRM, Inc</a>. All rights reserved. -->
